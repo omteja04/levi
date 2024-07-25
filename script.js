2a1ed8a8093a088b771e4c5bacd1adaed40daef9
omteja04 <22P31A0539@acet.ac.in>
2024-07-25 20:42:58 +0530

    script

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('urlForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const longUrl = document.getElementById('longUrl').value;
+    const apiUrl = 'https://8oxqs9zlt6.execute-api.us-east-1.amazonaws.com/dev/shorten';
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ longURL: longUrl })
+    })
+        .then(response => response.json())
+        .then(data => {
+            const shortUrl = `https://omteja04.github.io/levi/?code=${data.body.shortURL}`;
+            document.getElementById('shortUrlDisplay').innerHTML = `<a href="${shortUrl}" target="_blank">${shortUrl}</a>`;
+        })
+        .catch(error => console.error('Error:', error));
+});
